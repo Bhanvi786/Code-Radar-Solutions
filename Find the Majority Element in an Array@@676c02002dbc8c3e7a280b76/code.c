2cc6a35cd6e5cc0936267dc6b91c63deb9804276
Bhanvi786 <bhanvisahni@gmail.com>
2025-03-17 20:23:08 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,47 +1,46 @@
-// Your code here...
-#include<stdio.h>
-int main(){
-int n,count,i;
-scanf("%d",&n);
-int arr[n],freq[n];
-for(int i=0;i<n;i++)
-{
-    scanf("%d",&arr[i]);
-    freq[i]=-1;
-}
-for(int i=0;i<n;i++)
-{
-    if(freq[i]==-1)
-    {
-        int count=1;
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n], freq[n];
+
+    // Initialize frequency array
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        freq[i] = -1;  // Mark all elements as unprocessed
     }
-    for(int j=i+1;j<n;j++)
-    {
-        if(arr[i]==arr[j])
-        {
-            count++;
-            freq[j]=0;
+
+    // Count frequency of each element
+    for(int i = 0; i < n; i++) {
+        if(freq[i] == -1) {  // Only process uncounted elements
+            int count = 1;   // Reset count for new element
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    count++;
+                    freq[j] = 0; // Mark duplicate as processed
+                }
+            }
+            freq[i] = count; // Store frequency of current element
         }
     }
-    freq[i]=count;
-    
-}
-int flag=0,a;
-for(int i=0;i<n;i++)
-{
-    if(freq[i]>n/2)
-    {
-        a=arr[i];
-        flag=1;
-        break;
+
+    // Find majority element
+    int flag = 0, a;
+    for(int i = 0; i < n; i++) {
+        if(freq[i] > n / 2) { // Check if it appears more than n/2 times
+            a = arr[i];
+            flag = 1;
+            break;
+        }
     }
-}
-if(flag)
-{
-printf("%d",a);
-}
-else
-{
-    printf("-1");
-}
+
+    // Print result
+    if(flag) {
+        printf("%d\n", a);
+    } else {
+        printf("-1\n"); // No majority element
+    }
+
+    return 0;
 }
